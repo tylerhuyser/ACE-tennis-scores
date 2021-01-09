af535afce8c7ca3f291092b02c0ebe8ebe092e16
Tyler Huyser <tyler.huyser@gmail.com>
2021-01-09 15:11:18 -0500

    complete tournaments api formulas

--- client/src/utils/tournaments.js
+++ client/src/utils/tournaments.js
@@ -10,7 +10,22 @@ export const getCurrentTournaments = async () => {
   return resp.data
 }
 
+export const getDetailedTournamentInfo = async (id) => {
+  const resp = await api.get(`/tournaments/${id}/info.json`)
+  return resp.data
+}
+
 export const getTournamentSummary = async (id) => {
   const resp = await api.get(`/tournaments/${id}/summaries.json`)
+  return resp.data
+}
+
+export const getTournamentSchedule = async (id) => {
+  const resp = await api.get(`/tournaments/${id}/schedule.json`)
+  return resp.data
+}
+
+export const getTournamentResults = async (id) => {
+  const resp = await api.get(`/tournaments/${id}/resutls.json`)
   return resp.data
 }
