713ce2ffe7656449fd258001e99fdbd561b603c1
Tyler Huyser <tyler.huyser@gmail.com>
2021-01-09 00:13:00 -0500

    Built Switch for Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+// Screens
+import Home from './screens/Home'
+import Calendar from './screens/Calendar'
+import Rankings from './screens/Rankings'
+import TournamentDetail from './screens/TournamentDetail'
+import MatchDetail from './screens/MatchDetail'
+import PlayerDetail from './screens/PlayerDetail'
 
 function App () {
 
+  const [ searchQuery, setSearchQuery ] = useState("")
+
   return (
     <div className="App">
+
+      <Switch>
+
+        <Home />
+
+        <Calendar />
+
+        <Rankings />
+
+        <TournamentDetail />
+
+        <MatchDetail />
+
+        <PlayerDetail />
+
+      </Switch>
 
     </div>
   );
