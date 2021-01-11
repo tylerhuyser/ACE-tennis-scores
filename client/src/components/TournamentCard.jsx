7eda5bfcb7cfe9f7b85c78e9dec13fea7cdddf3f
Tyler Huyser <tyler.huyser@gmail.com>
2021-01-11 14:57:06 -0500

    styled tournament cards and filtered current tournament data passed from Home screen

--- client/src/components/TournamentCard.jsx
+++ client/src/components/TournamentCard.jsx
@@ -1,31 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-export default function TournamentCard(props) {
+import './TournamentCard.css'
+
+export default function TournamentCard (props) {
+
+  const [ tournamentName, setTournamentName ] = useState("")
+  const [ tournamentCategory, setTournamentCategory ] = useState("")
+  const [ tournamentCategoryIcon, setTournamentCategoryIcon ] = useState("")
   
   const { tournament, index } = props;
 
-  const parseTournamentName = (tournament) => {
-    const splitTournamentName = tournament.name.split(",")
-    const isolatedTournamentNameAndTier = splitTournamentName[0].split(" ")
-    const isolatedTournamentName = isolatedTournamentNameAndTier.slice(1)
-    if (tournament.name.includes("WTA") || tournament.name.includes("ATP")) {
-      const tournamentName = isolatedTournamentName.join(" ")
-      console.log(tournamentName)
-      return tournamentName
-    } if (tournament.name.includes("ITF")) {
-      const isolatedITFTournamentName = isolatedTournamentName.slice(0, -1)
-      const tournamentName = isolatedITFTournamentName.join(" ")
-      console.log(tournamentName)
-      return tournamentName
-    }
-  }
+  useEffect(() => {
+    
+    const parseTournamentInfo = (tournament) => {
 
-  const tournamentName = parseTournamentName(tournament)
+      const splitTournamentName = tournament.name.split(",")
+      const isolatedTournamentNameAndTier = splitTournamentName[0].split(" ")
+      const isolatedTournamentTier = isolatedTournamentNameAndTier[0]
+      const isolatedTournamentName = isolatedTournamentNameAndTier.slice(1)
+    
+      if (tournamentName.includes("doubles")) {
+        return
+      } else if (tournament.name.includes("WTA")) {
+        setTournamentName(isolatedTournamentName.join(" "))
+        setTournamentCategory(isolatedTournamentTier)
+        setTournamentCategoryIcon("https://images.firstpost.com/wp-content/uploads/2020/12/wta-logo-640.png?impolicy=website&width=1200&height=800")
+      }  else if (tournament.name.includes("ATP")) {
+        setTournamentName(isolatedTournamentName.join(" "))
+        setTournamentCategory(isolatedTournamentTier)
+        setTournamentCategoryIcon("https://logodix.com/logo/1903236.png")
+      } else if (tournament.name.includes("ITF")) {
+        const isolatedITFTournamentName = isolatedTournamentName.slice(0, -1)
+        setTournamentName(isolatedITFTournamentName.join(" "))
+        setTournamentCategory(isolatedTournamentTier)
+        setTournamentCategoryIcon("https://logodix.com/logo/1903236.png")
+      }
+    }
+    parseTournamentInfo(tournament)
+  }, [])
+
 
   return (
     <div className="tournament-card-container">
-      <p className="tournament-name">{tournamentName}</p>
-      <p className="tournament-category"></p>
+      <p className="tournament-card-name">{tournamentName}</p>
+      <div className="tournament-category-container">
+        <p className="tournament-category">{tournamentCategory}</p>
+        <img className="tournament-category-icon" src={tournamentCategoryIcon} />
+      </div>
     </div>
   )
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -2,17 +2,21 @@ import React from 'react';
 
 import TournamentCard from '../components/TournamentCard'
 
+import './Home.css'
+
 
 export default function Home(props) {
 
   const { currentTournaments } = props
 
-  const tournaments = currentTournaments && currentTournaments?.map((tournament, index) => (
+  const tournaments = currentTournaments?.filter((tournament) => (!tournament.name.toLowerCase().includes("itf") || (!tournament.name.toLowerCase().includes("doubles")))).map((tournament, index) => (
+
     <TournamentCard 
       tournament={tournament}
       index={index}
       key={tournament.id}
     />
+
   ))
   
   return (
