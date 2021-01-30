fa9fb49fd39c9dff877c069b1d517e5701f33e40
Tyler Huyser <tyler.huyser@gmail.com>
2021-01-30 03:28:38 -0500

    removed unnecessary components and begun to write Player Detail component

--- client/src/components/PlayerCard.jsx
+++ client/src/components/PlayerCard.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export default function PlayerCard(props) {
 
   const { player, key } = props
+
+  const history = useHistory()
+
+  const handlePlayerDetails = (id) => {
+
+    localStorage.setItem('currentPlayer', JSON.stringify(player))
+    history.pushState(`/player/${player.id}`)
+  }
   
   return (
-    <div className="player-container" key={key}>
+    <div className="player-container" key={key} onClick={(e) => handlePlayerDetails(e)}>
       <p className="player-ranking"></p>
       <p className="player-name">{player.name}</p>
       <p className="player-country"></p>
--- client/src/screens/PlayerDetail.jsx
+++ client/src/screens/PlayerDetail.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
+
+import Loader from '../components/Loader'
+
+import './PlayerDetail.css'
+
+import {
+  getPlayer,
+  getPlayerResults,
+  getPlayerSchedule
+} from "../utils/players"
 
 export default function PlayerDetail(props) {
+
+  const [playerData, setPlayerData] = useState(null)
+  const [playerResults, setPlayerResults] = useState(null)
+  const [playerSchedule, setPlayerSchedule] = useState(null)
+
+  useEffect(() => {
+    const currentPlayer = localStorage.getItem('currentPlayer')
+
+    if (currentPlayer === undefined) {
+      
+    } else {
+      setPlayerData(currentPlayer)
+    }
+  },[])
   
   return (
     <div className="player-detail-container">
+
+      { loaded ? 
+      
+        <Loader />
+
+        :
+
+        <>
+          
+        </>
+      
+      }
       
     </div>
   )
