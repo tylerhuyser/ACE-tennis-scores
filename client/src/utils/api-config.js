d5b18d41c35ab6a51ebcb7c738cbe85bc2aa6e9c
Tyler Huyser <tyler.huyser@gmail.com>
2021-03-02 15:54:05 -0500

    replaced https with cors-anywhere-herokuapp

--- client/src/utils/api-config.js
+++ client/src/utils/api-config.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const key = `?api_key=${process.env.REACT_APP_API_KEY}`
 
-const baseUrl = process.env.NODE_ENV === 'production' ? `https://api.sportradar.us/tennis-t2/en` : 'http://localhost:8080/http://api.sportradar.us/tennis-t2/en'
+const baseUrl = process.env.NODE_ENV === 'production' ? `https://cors-anywhere.herokuapp.com/http://api.sportradar.us/tennis-t2/en` : 'http://localhost:8080/http://api.sportradar.us/tennis-t2/en'
 
 const api = axios.create({
   baseURL: baseUrl
