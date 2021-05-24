68f41674b7ba9fe289146d2a8caafe0a48508ac2
Tyler Huyser <tyler.huyser@gmail.com>
2021-05-24 15:38:45 -0400

    added routes for rankings model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  resources :rankings
   post '/auth/login', to: 'authentication#login'
   get '/auth/verify', to: 'authentication#verify'
   resources :users
+
+  get '/rankings', to: 'rankings#index'
+  post '/rankings', to: 'rankings#create'
+  patch '/rankings', to: 'rankings#update'
+
+  resources :rankings
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
