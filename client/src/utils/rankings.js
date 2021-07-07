689e89b5bf272b043ac12eab1e5bd6320738f9d8
Tyler Huyser <tyler.huyser@gmail.com>
2021-07-07 12:23:26 -0400

    removed console logs that revealed keys in rankings.js file

--- client/src/utils/rankings.js
+++ client/src/utils/rankings.js
@@ -41,21 +41,7 @@ export const herokuRankings = async () => {
     }
   }
 
-  // const tokenData = await axios.post(`https://api.allorigins.win/raw?url=${process.env.REACT_APP_HEROKU_URL}auth/login`, {
-  //   "authentication": {
-  //     "username": `${process.env.REACT_APP_POSTGRES_USERNAME}`,
-  //     "password": `${process.env.REACT_APP_POSTGRES_PASSWORD}`
-  //   },
-  //   "headers": {
-  //     'Accept': 'application/json',
-  //     'Content-Type': 'application/json',
-  //   }
-  // })
-
   const tokenData = await axios(`http://localhost:8080/${process.env.REACT_APP_HEROKU_URL}auth/login`, authConfig)
-
-  console.log(tokenData)
-  console.log(tokenData.data.token)
 
   const config = {
     method: 'get',
@@ -65,7 +51,6 @@ export const herokuRankings = async () => {
   }
 
   const resp = await axios(`http://localhost:8080/${process.env.REACT_APP_HEROKU_URL}rankings`, config)
-  console.log(resp)
   return resp.data
   
 }
